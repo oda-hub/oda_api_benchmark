c8d07b28ec2bb838ab3f4049765499d90b691e77
Volodymyr <vladimir.savchenko@gmail.com>
2019-05-28 16:47:43 +0200

    only use heasoft

---
+++ init.sh
@@ -0,0 +1,10 @@
+HOME_OVERRRIDE=/tmp/home
+export HOME=${HOME_OVERRRIDE:-/home/integral}
+
+export HEADAS=/opt/heasoft/x86_64-pc-linux-gnu-libc2.17/
+
+[ -s $HEADAS/headas-init.sh ] && 
+    . $HEADAS/headas-init.sh
+
+
+source /etc/pyenvrc
